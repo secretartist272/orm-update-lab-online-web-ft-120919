34eb832d8d995d9feb1c26e252e9401427e30dbf
Jacqueline Freeland <secretartist272@gmail.com>
2020-02-02 21:57:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,20 +29,7 @@ class Student
     DB[:conn].execute(sql)
   end
   
-   def save
-    if self.id 
-      self.update
-    else
-      sql = <<-SQL
-        INSERT INTO students (name, grade) 
-        VALUES (?, ?)
-      SQL
-
-      DB[:conn].execute(sql, self.name, self.grade)
-      
-      DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-    end
-  end
+  
   
   def self.update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
